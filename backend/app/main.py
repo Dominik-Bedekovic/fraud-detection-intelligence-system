957356fd85835f59cc74fe84a67501db1430f108
Dominik Bedekovic <dominik.bedekovic@gmail.com>
2026-04-12 23:05:23 +0200

    Fixed bug where the table wasnt showing properly because the csv file uses tab instead of commas for separation

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def show_info():
-    dataFrame = pandas.read_csv("test.csv") 
+    dataFrame = pandas.read_csv("test.csv", sep="\t") 
+
+    dataFrame = dataFrame[["step","type","amount","oldbalanceOrg","newbalanceOrig","oldbalanceDest","newbalanceDest",]]
+
     htmlFrame = dataFrame.to_html()
     return render_template("home.html", table=htmlFrame)
 if __name__ == '__main__':
