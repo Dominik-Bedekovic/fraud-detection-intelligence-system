27e87b9efdc00c0a248d28f811e9b58ec275f6c6
Dominik Bedekovic <dominik.bedekovic@gmail.com>
2026-04-15 00:55:20 +0200

    Fixed semantic issues in home.html and made a backend for printing out the filled out text form

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,15 +1,21 @@
-from flask import Flask, render_template
-import pandas
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def show_info():
-    dataFrame = pandas.read_csv("test.csv", sep="\t") 
+@app.route("/", methods=['GET', 'POST'])
+def show_input():
+    if request.method == 'POST':
 
-    dataFrame = dataFrame[["step","type","amount","oldbalanceOrg","newbalanceOrig","oldbalanceDest","newbalanceDest",]]
+        step = request.form["step"]
+        type = request.form["type"]
+        amount = request.form["amount"]
+        oldbalanceOrg = request.form["oldBalanceOrig"]
+        newbalanceOrig = request.form["newBalanceOrig"]
+        oldbalanceDest = request.form["oldBalanceDest"]
+        newbalanceDest = request.form["newBalanceDest"]
 
-    htmlFrame = dataFrame.to_html()
-    return render_template("home.html", table=htmlFrame)
+        return f"{step}, {type}, {amount}, {oldbalanceOrg}, {newbalanceOrig}, {oldbalanceDest}, {newbalanceDest}"
+
+    return render_template("home.html")
 if __name__ == '__main__':
     app.run(debug=True)
