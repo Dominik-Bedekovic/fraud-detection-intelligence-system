a64ecb08770c580ec69c44f49d0ab61fd6a7c469
Dominik Bedekovic <dominik.bedekovic@gmail.com>
2026-04-15 02:09:19 +0200

    Made backend and frontend that connects with the ml model and outputs the probability result

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,20 +1,33 @@
 from flask import Flask, render_template, request
+import pandas as pd
+import joblib
 
 app = Flask(__name__)
+
+bundle = joblib.load("backend/model/fraud_pipeline.joblib")
+pipeline = bundle["pipeline"]
 
 @app.route("/", methods=['GET', 'POST'])
 def show_input():
     if request.method == 'POST':
 
-        step = request.form["step"]
-        type = request.form["type"]
-        amount = request.form["amount"]
-        oldbalanceOrg = request.form["oldBalanceOrig"]
-        newbalanceOrig = request.form["newBalanceOrig"]
-        oldbalanceDest = request.form["oldBalanceDest"]
-        newbalanceDest = request.form["newBalanceDest"]
+        data = {
+            "step": int(request.form["step"]),
+            "type": request.form["type"],
+            "amount": float(request.form["amount"]),
+            "oldbalanceOrg": float(request.form["oldBalanceOrig"]),
+            "newbalanceOrig": float(request.form["newBalanceOrig"]),
+            "oldbalanceDest": float(request.form["oldBalanceDest"]),
+            "newbalanceDest": float(request.form["newBalanceDest"]) 
+        }
 
-        return f"{step}, {type}, {amount}, {oldbalanceOrg}, {newbalanceOrig}, {oldbalanceDest}, {newbalanceDest}"
+        df = pd.DataFrame([data])
+
+        probability = pipeline.predict_proba(df)[0][1]
+
+
+
+        return f"Fraud probability: {probability * 100:.2f}%"
 
     return render_template("home.html")
 if __name__ == '__main__':
